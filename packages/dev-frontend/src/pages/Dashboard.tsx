87d2f84b23d1a006f8d7612a9ef857df1062e645
aptt <git@aptt.ch>
2024-03-25 16:03:50 +0100

    feat(dev-frontend): 🎸 remove price manager from dashboard

--- packages/dev-frontend/src/pages/Dashboard.tsx
+++ packages/dev-frontend/src/pages/Dashboard.tsx
@@ -3,7 +3,6 @@ import { Container } from "theme-ui";
 import { Trove } from "../components/Trove/Trove";
 import { Stability } from "../components/Stability/Stability";
 import { SystemStats } from "../components/SystemStats";
-import { PriceManager } from "../components/PriceManager";
 import { Staking } from "../components/Staking/Staking";
 import { BondsTable } from "../components/Bonds/BondsTable";
 
@@ -18,7 +17,6 @@ export const Dashboard: React.FC = () => (
 
     <Container variant="right">
       <SystemStats />
-      <PriceManager />
     </Container>
   </Container>
 );
