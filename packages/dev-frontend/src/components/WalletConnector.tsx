ac97b7cb7c9b024c07bc9def18590a60d5f0aacd
aptt <git@aptt.ch>
2024-04-24 16:00:32 +0200

    feat(dev-frontend): 🎸 add text to start screen

--- packages/dev-frontend/src/components/WalletConnector.tsx
+++ packages/dev-frontend/src/components/WalletConnector.tsx
@@ -1,5 +1,6 @@
+/** @jsxImportSource theme-ui */
 import { ConnectKitButton } from "connectkit";
-import { Box, Button, Flex } from "theme-ui";
+import { Box, Button, Flex, Heading, Paragraph } from "theme-ui";
 import { Icon } from "./Icon";
 
 type WalletConnectorProps = {
@@ -13,8 +14,34 @@ export const WalletConnector: React.FC<WalletConnectorProps> = ({ children }) =>
         connectKit.isConnected ? (
           children
         ) : (
-          <Flex sx={{ height: "100vh", justifyContent: "center", alignItems: "center" }}>
-            <Button onClick={connectKit.show}>
+          <Flex
+            sx={{
+              minHeight: "100vh",
+              justifyContent: "center",
+              flexDirection: "column",
+              marginInline: "clamp(2rem, 100%, 50% - 16rem)",
+              paddingBlock: "4rem"
+            }}
+          >
+            <hgroup>
+              <Heading sx={{ color: "primary" }}>HLiquity.finance</Heading>
+
+              <Paragraph sx={{ marginTop: "0.5rem", fontSize: "1.125rem" }}>
+                DeFi Borrowing on the Hedera Network
+              </Paragraph>
+            </hgroup>
+
+            <Paragraph sx={{ marginTop: "2rem", fontSize: "1.125rem" }}>
+              A front-end for HLiquity &ndash; a decentralized borrowing protocol for{" "}
+              <span sx={{ color: "secondary", fontWeight: "bold" }}>interest-free loans</span> paid
+              out in HCHF, pegged to the{" "}
+              <span sx={{ color: "secondary", fontWeight: "bold" }}>
+                strong and reliable Swiss Franc
+              </span>
+              .
+            </Paragraph>
+
+            <Button onClick={connectKit.show} sx={{ marginTop: "4rem", alignSelf: "center" }}>
               <Icon name="plug" size="lg" />
               <Box sx={{ ml: 2 }}>Connect wallet</Box>
             </Button>
