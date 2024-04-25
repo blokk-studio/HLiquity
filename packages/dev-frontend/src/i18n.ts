f73ed576a9f5a482dffe6e70482a6b5a0dc97b5b
aptt <git@aptt.ch>
2024-04-25 11:06:10 +0200

    feat(dev-frontend): 🎸 export translation fn & add better replacement fn

--- packages/dev-frontend/src/i18n.ts
+++ packages/dev-frontend/src/i18n.ts
@@ -16,4 +16,22 @@ i18n.use(initReactI18next).init({
   }
 });
 
+export const t = i18n.t;
+
+export const replace = (key: string, options: Record<string, any>) => {
+  const translationString = t(key);
+  const splitTranslationString = translationString.split(/{{([^}]+)}}/);
+  const replaced = splitTranslationString.map((stringOrPlaceholder, index) => {
+    if (index % 2 === 1) {
+      const replacement = options[stringOrPlaceholder];
+
+      return replacement ?? stringOrPlaceholder;
+    }
+
+    return stringOrPlaceholder;
+  });
+
+  return replaced;
+};
+
 export default i18n;
