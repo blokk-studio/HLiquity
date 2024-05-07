4277f3632d543a1c806bc74e01a74eb4d01a989d
aptt <git@aptt.ch>
2024-05-07 16:01:27 +0200

    feat(dev-frontend): 🎸 remove bonds context & table

--- packages/dev-frontend/src/pages/Dashboard.tsx
+++ packages/dev-frontend/src/pages/Dashboard.tsx
@@ -4,12 +4,10 @@ import { Trove } from "../components/Trove/Trove";
 import { Stability } from "../components/Stability/Stability";
 import { SystemStats } from "../components/SystemStats";
 import { Staking } from "../components/Staking/Staking";
-import { BondsTable } from "../components/Bonds/BondsTable";
 
 export const Dashboard: React.FC = () => (
   <Container variant="columns">
     <Container variant="left">
-      <BondsTable />
       <Trove />
       <Stability />
       <Staking />
