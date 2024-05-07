56e2bfed8e88e14cb56c65d89c2b7b38328fe415
aptt <git@aptt.ch>
2024-05-07 19:02:46 +0200

    feat(dev-frontend): 🎸 open blokk links in new tab

--- packages/dev-frontend/src/components/Imprint.tsx
+++ packages/dev-frontend/src/components/Imprint.tsx
@@ -2,18 +2,25 @@
 import React, { useEffect, useState } from "react";
 
 export const Imprint: React.FC = () => {
-    const [email, setEmail] = useState('')
-    useEffect(() => {
-        setEmail(atob('aGVsbG9AYmxva2suc3R1ZGlv'))
-    }, [])
-    
-    return (
-  <address sx={{ display: "flex", flexDirection: "column", fontStyle: "normal" }}>
-    <span>blokk.</span>
-    <span>Dammstrasse 16</span>
-    <span>6300 Zug</span>
-    <span>Switzerland</span>
-    <a href="https://blokk.studio/">https://blokk.studio/</a>
-    {email && <a href="mailto:hello@blokk.studio">hello@blokk.studio</a>}
-  </address>
-)};
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    setEmail(atob("aGVsbG9AYmxva2suc3R1ZGlv"));
+  }, []);
+
+  return (
+    <address sx={{ display: "flex", flexDirection: "column", fontStyle: "normal" }}>
+      <span>blokk.</span>
+      <span>Dammstrasse 16</span>
+      <span>6300 Zug</span>
+      <span>Switzerland</span>
+      <a href="https://blokk.studio/" target="_blank">
+        https://blokk.studio/
+      </a>
+      {email && (
+        <a href="mailto:hello@blokk.studio" target="_blank">
+          hello@blokk.studio
+        </a>
+      )}
+    </address>
+  );
+};
