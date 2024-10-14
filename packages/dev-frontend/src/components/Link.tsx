5634fa736d47d061887404a00a24c2032c43c1f0
aptt <git@aptt.ch>
2024-10-14 14:50:05 +0200

    fix(frontend): 🐛 force correct router link type

--- packages/dev-frontend/src/components/Link.tsx
+++ packages/dev-frontend/src/components/Link.tsx
@@ -1,10 +1,15 @@
 import { NavLink as RouterLink, NavLinkProps as RouterLinkProps } from "react-router-dom";
 import { NavLink as ThemeUINavLink, NavLinkProps as ThemeUILinkProps } from "theme-ui";
+import { FunctionComponent } from "react";
 
 type CombinedProps = ThemeUILinkProps & RouterLinkProps;
 
+// 'RouterLink' cannot be used as a JSX component.
+//   Its return type 'ReactElement<any, string | JSXElementConstructor<any>> | null' is not a valid JSX element.
+const RouterLinkWithFixedType = RouterLink as unknown as FunctionComponent<RouterLinkProps>;
+
 const ExactLink: React.FC<CombinedProps> = props => {
-  return <RouterLink exact {...props} />;
+  return <RouterLinkWithFixedType exact {...props} />;
 };
 
 export const Link: React.FC<CombinedProps> = props => {
