d13bad9def9f95afe4eb983911f71cdc855cac2a
aptt <git@aptt.ch>
2024-10-14 12:08:15 +0200

    feat(frontend): 🎸 allow unstaking all lp despite disabled lp staking

--- packages/dev-frontend/src/components/Mine/views/Disabled/Disabled.tsx
+++ packages/dev-frontend/src/components/Mine/views/Disabled/Disabled.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Card, Heading, Box, Link, Flex, Paragraph } from "theme-ui";
+import { Card, Heading, Box, Link, Flex } from "theme-ui";
 import { LiquityStoreState } from "@liquity/lib-base";
 import { useLiquitySelector } from "@liquity/lib-react";
 import { StaticRow } from "../../../Trove/Editor";
 import { GT, LP } from "../../../../strings";
 import { ClaimReward } from "../Active/ClaimReward";
+import { useLiquity } from "../../../../hooks/LiquityContext";
+import { useLoadingState } from "../../../../loading_state";
+import { LoadingButton } from "../../../LoadingButton";
+import { ActionDescription } from "../../../ActionDescription";
 
 const selector = ({
   liquidityMiningStake,
@@ -21,12 +25,30 @@ export const Disabled: React.FC = () => {
     useLiquitySelector(selector);
   const hasStake = !liquidityMiningStake.isZero;
   const poolShare = liquidityMiningStake.mulDiv(100, totalStakedUniTokens);
+  const liquityContext = useLiquity();
+  const loadableUnstakeUniTokens = useLoadingState(async () => {
+    const result = await liquityContext.liquity.send.unstakeUniTokens(liquidityMiningStake);
+    await result.waitForReceipt();
+  }, [hasStake]);
 
   return (
     <Card>
       <Heading>SaucerSwap HBAR/HCHF-LP Staking</Heading>
       <Box sx={{ p: [2, 3] }}>
-        {hasStake ? (
+        <ActionDescription>
+          The SaucerSwap HLQT Token distribution has stopped 18 weeks after launch. You can still
+          claim your rewards and unstake your active stake, but won't be able to stake anymore.{" "}
+          <Link
+            href="https://docs.hliquity.org/fundamentals/hlqt-tokenomics#community-40"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ color: "info" }}
+          >
+            More on HLQT tokenomics.
+          </Link>
+        </ActionDescription>
+
+        {hasStake && (
           <>
             <Box sx={{ border: 1, pt: 3, borderRadius: 3 }}>
               <StaticRow
@@ -52,21 +74,15 @@ export const Disabled: React.FC = () => {
 
             <Flex variant="layout.actions">
               <ClaimReward />
+
+              <LoadingButton
+                loading={loadableUnstakeUniTokens.state === "pending"}
+                onClick={loadableUnstakeUniTokens.call}
+              >
+                Unstake all LP ({liquidityMiningStake.prettify(2)})
+              </LoadingButton>
             </Flex>
           </>
-        ) : (
-          <Paragraph>
-            The SaucerSwap HLQT Token distribution has stopped 18 weeks after launch. You can still
-            claim rewards from your active stake, but won't be able to stake anymore.{" "}
-            <Link
-              href="https://docs.hliquity.org/fundamentals/hlqt-tokenomics#community-40"
-              target="_blank"
-              rel="noopener noreferrer"
-              sx={{ color: "info" }}
-            >
-              More on HLQT tokenomics.
-            </Link>
-          </Paragraph>
         )}
       </Box>
     </Card>
