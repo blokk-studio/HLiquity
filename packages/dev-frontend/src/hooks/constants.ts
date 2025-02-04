6131ccd9794d96c88054eedde0edf2a92c958cec
aptt <git@aptt.ch>
2025-02-04 15:22:22 +0100

    fix(dev-frontend): 🐛 calculate constants once & look them up in hook
    fixes a reactivity issue caused by creating a new object with JSON.parse
    for every re-render

--- packages/dev-frontend/src/hooks/constants.ts
+++ packages/dev-frontend/src/hooks/constants.ts
@@ -1,11 +1,19 @@
-import { getConstantsFromJsonObjectString } from "@liquity/lib-base";
-import { enabledChainIds } from "../configuration/chains";
+import { Constants, getConstantsFromJsonObjectString } from "@liquity/lib-base";
+import { chains, enabledChainIds } from "../configuration/chains";
 import { deployments } from "../configuration/deployments";
 import { useMultiWallet } from "../multi_wallet";
 
 const setOfEnabledChainIds = new Set(enabledChainIds);
 export const enabledDeployments = deployments.filter(deployment =>
   setOfEnabledChainIds.has(deployment.chainId)
+);
+const chainConstants: Record<number, Constants> = Object.fromEntries(
+  chains.map(chain => {
+    const constantsJsonString = import.meta.env[`VITE_CHAIN_${chain.id}_CONSTANTS`];
+    const constants = getConstantsFromJsonObjectString(constantsJsonString);
+
+    return [chain.id, constants];
+  })
 );
 
 /** returns the constants for the chain that is currently selected in wagmi or hashconnect */
@@ -19,8 +27,7 @@ export const useConstants = () => {
     throw new Error(errorMessage);
   }
 
-  const constantsJsonString = import.meta.env[`VITE_CHAIN_${chain.id}_CONSTANTS`];
-  const constants = getConstantsFromJsonObjectString(constantsJsonString);
+  const constants = chainConstants[chain.id];
 
   return constants;
 };
