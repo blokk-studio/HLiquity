dd5663cbc967fc8d5530bff07bdbcc164a4dd805
aptt <git@aptt.ch>
2025-03-11 11:01:28 +0100

    feat(frontend): 🎸 add button to wallet not connected info component

--- packages/dev-frontend/src/components/WalletNotConnectedInfo.tsx
+++ packages/dev-frontend/src/components/WalletNotConnectedInfo.tsx
@@ -1,9 +1,22 @@
+import { Button } from "theme-ui";
 import { ActionDescription } from "./ActionDescription";
+import { useMultiWallet } from "../multi_wallet";
 
 export const WalletNotConnectedInfo: React.FC = () => {
+  const { showConnectionDialog } = useMultiWallet();
+
   return (
     <ActionDescription>
-      To interact with the HLiquity protocol, please connect your wallet.
+      To interact with the HLiquity protocol, please connect your wallet.{" "}
+      <Button
+        sx={{
+          display: "inline"
+        }}
+        variant="small"
+        onClick={showConnectionDialog}
+      >
+        Connect
+      </Button>
     </ActionDescription>
   );
 };
