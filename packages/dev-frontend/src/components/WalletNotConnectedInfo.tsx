d82509cb2c44e4dc042aa31b217eb539cd663b41
aptt <git@aptt.ch>
2025-03-11 10:31:08 +0100

    feat(frontend): 🎸 add info text component for when wallet isn't connected

---
+++ packages/dev-frontend/src/components/WalletNotConnectedInfo.tsx
@@ -0,0 +1,9 @@
+import { ActionDescription } from "./ActionDescription";
+
+export const WalletNotConnectedInfo: React.FC = () => {
+  return (
+    <ActionDescription>
+      To interact with the HLiquity protocol, please connect your wallet.
+    </ActionDescription>
+  );
+};
