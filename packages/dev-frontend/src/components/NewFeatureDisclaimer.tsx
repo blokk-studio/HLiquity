ae38cda3dc48fb0f9a26a48e11a315e609a151ad
aptt <git@aptt.ch>
2025-03-13 13:33:36 +0100

    feat(frontend): 🎸 add optional tooltip prop for tooltip content

--- packages/dev-frontend/src/components/NewFeatureDisclaimer.tsx
+++ packages/dev-frontend/src/components/NewFeatureDisclaimer.tsx
@@ -3,38 +3,33 @@ import { Icon } from "./Icon";
 import { Tooltip } from "./Tooltip";
 import { Text } from "theme-ui";
 import styles from "./NewFeatureDisclaimer.module.css";
+import { ReactNode } from "react";
 
-export const NewFeatureDisclaimer: React.FC = () => {
+export const NewFeatureDisclaimer: React.FC<{ tooltip?: ReactNode }> = props => {
+  const spinnerElement = (
+    <span
+      role="presentation"
+      sx={{
+        display: "grid",
+        placeContent: "center"
+      }}
+      className={styles.spinAnimation}
+    >
+      <span
+        role="presentation"
+        sx={{
+          display: "grid",
+          placeContent: "center"
+        }}
+        className={styles.scaleAnimation}
+      >
+        <Icon name="certificate" size="lg" color="#ff755f" />
+      </span>
+    </span>
+  );
   return (
     <div role="presentation" sx={{ display: "inline-block", pointerEvents: "all" }}>
-      <Tooltip
-        message={
-          <Text>
-            <em sx={{ fontStyle: "normal", fontWeight: "bold" }}>New Feature, tested on TestNet.</em>{" "}
-            Now live, but needs validation on MainNet.
-          </Text>
-        }
-      >
-        <span
-          role="presentation"
-          sx={{
-            display: "grid",
-            placeContent: "center"
-          }}
-          className={styles.spinAnimation}
-        >
-          <span
-            role="presentation"
-            sx={{
-              display: "grid",
-              placeContent: "center"
-            }}
-            className={styles.scaleAnimation}
-          >
-            <Icon name="certificate" size="lg" color="#ff755f" />
-          </span>
-        </span>
-      </Tooltip>
+      {props.tooltip ? <Tooltip message={props.tooltip}>{spinnerElement}</Tooltip> : spinnerElement}
     </div>
   );
 };
