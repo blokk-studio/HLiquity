12169337920b1b2236260f982c1d9d575ad28e14
aptt <git@aptt.ch>
2025-10-02 12:26:11 +0200

    feat(frontend): 🎸 show redemptions from all accounts & highlight own

--- packages/dev-frontend/src/pages/RedemptionsPage.tsx
+++ packages/dev-frontend/src/pages/RedemptionsPage.tsx
@@ -1,6 +1,17 @@
 /** @jsxImportSource theme-ui */
 import React, { useEffect, useMemo, useState } from "react";
-import { Box, Card, Container, Heading, Paragraph, Spinner, Text } from "theme-ui";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Paragraph,
+  Spinner,
+  Text
+} from "theme-ui";
 import { Decimal } from "@liquity/lib-base";
 import { useMirrorNodeClient } from "../components/MirrorNodeClientContext";
 import { useDeployment } from "../hooks/deployments";
@@ -11,6 +22,10 @@ import { Tooltip } from "../components/Tooltip";
 import { components } from "../../.mirror-node";
 import { ActionDescription } from "../components/ActionDescription";
 import { ErrorDescription } from "../components/ErrorDescription";
+import { useSelectedChain } from "../components/chain_context";
+import { useLocation } from "react-router";
+import { Icon } from "../components/Icon";
+import { Link as RouterLink } from "../components/Link";
 
 /** hard-coded version of:
  * const { keccak256 } = require("js-sha3");
@@ -24,6 +39,7 @@ const redeemCollateralFunctionParametersStart = `0x${redeemCollateralSelector}`;
 interface Redemption {
   transactionId: string;
   timestamp: string;
+  accountIdString: string;
   amountOfHchf: Decimal;
   amountOfHbar: Decimal;
 }
@@ -33,16 +49,33 @@ const shortDateTimeFormat = new Intl.DateTimeFormat(navigator.language, {
   timeStyle: "short"
 });
 
-const longDateTimeFormat = new Intl.DateTimeFormat(navigator.language, {
+const fullDateTimeFormat = new Intl.DateTimeFormat(navigator.language, {
   dateStyle: "full",
   timeStyle: "full"
 });
+
+const getTimestampFromHref = (href: string) => {
+  const timestampParam = new URL(href, window.location.origin).searchParams.get("timestamp");
+  if (!timestampParam) {
+    return;
+  }
+
+  const timestampString = timestampParam.replace(/^\w+:/g, "");
+  const timestamp = parseFloat(timestampString);
+
+  if (isNaN(timestamp)) {
+    return;
+  }
+
+  return timestampString;
+};
 
 export const RedemptionsPage: React.FC = () => {
   const [redemptions, setRedemptions] = useState<Redemption[] | Error | null>(null);
 
   const mirrorNodeClient = useMirrorNodeClient();
   const deployment = useDeployment();
+  const chain = useSelectedChain();
   const multiWallet = useMultiWallet();
   const store = useLiquitySelector(({ hchfTokenAddress }) => ({ hchfTokenAddress }));
   const accountIdString = useMemo(() => {
@@ -57,16 +90,37 @@ export const RedemptionsPage: React.FC = () => {
 
     return AccountId.fromString(multiWallet.addressDisplayText).toString();
   }, [multiWallet.addressDisplayText]);
+  const hchfTokenIdString = useMemo(() => {
+    if (!store.hchfTokenAddress) {
+      return;
+    }
+
+    return TokenId.fromEvmAddress(0, 0, store.hchfTokenAddress).toString();
+  }, [store.hchfTokenAddress]);
+  const [pagination, setPagination] = useState<{
+    previous?: string;
+    next?: string;
+  }>({});
+  const location = useLocation();
+  const urlTimestamp = useMemo(() => {
+    return getTimestampFromHref(`${location.pathname}${location.search}${location.hash}`);
+  }, [location]);
 
   useEffect(() => {
     const effect = async () => {
-      if (!deployment || !accountIdString) {
-        setRedemptions(new Error("Please connect your wallet."));
+      setRedemptions(null);
+      setPagination({});
+
+      if (!deployment || !hchfTokenIdString) {
+        setRedemptions(
+          new Error(
+            "The frontend is misconfigured. This should never happen, please contact support."
+          )
+        );
         return;
       }
 
-      const hchfTokenIdString = TokenId.fromEvmAddress(0, 0, store.hchfTokenAddress).toString();
-
+      const timestamp = urlTimestamp ?? Date.now() / 1000;
       const resultsResponse = await mirrorNodeClient.GET(
         "/api/v1/contracts/{contractIdOrAddress}/results",
         {
@@ -75,8 +129,7 @@ export const RedemptionsPage: React.FC = () => {
               contractIdOrAddress: deployment.addresses.troveManager
             },
             query: {
-              from: accountIdString,
-              internal: true
+              timestamp: [`lte:${timestamp}`]
             }
           }
         }
@@ -91,9 +144,11 @@ export const RedemptionsPage: React.FC = () => {
       }
       if (!resultsResponse.data?.results) {
         setRedemptions([]);
+        setPagination({});
         return;
       }
 
+      // fetch details for all transactions
       const redeemCollateralResults = resultsResponse.data.results.filter(
         (result): result is { timestamp: string } & typeof result => {
           return (
@@ -105,6 +160,11 @@ export const RedemptionsPage: React.FC = () => {
 
       const redemptionOrUndefined = await Promise.all(
         redeemCollateralResults.map(async (result): Promise<Redemption | undefined> => {
+          if (!result.from) {
+            return;
+          }
+
+          const accountIdString = AccountId.fromEvmAddress(0, 0, result.from).toString();
           const timestamp = result.timestamp;
 
           const timestampedTransactionsResponse = await mirrorNodeClient.GET(
@@ -112,7 +172,6 @@ export const RedemptionsPage: React.FC = () => {
             {
               params: {
                 query: {
-                  "account.id": accountIdString,
                   timestamp: [timestamp]
                 }
               }
@@ -196,6 +255,7 @@ export const RedemptionsPage: React.FC = () => {
           return {
             timestamp,
             transactionId,
+            accountIdString,
             amountOfHchf,
             amountOfHbar
           };
@@ -205,24 +265,154 @@ export const RedemptionsPage: React.FC = () => {
         (redemption): redemption is Redemption => !!redemption
       );
 
+      // pagination
+      let next: string | undefined = undefined;
+      if (resultsResponse.data.links?.next) {
+        const nextTimestamp = getTimestampFromHref(resultsResponse.data.links.next);
+
+        if (nextTimestamp) {
+          const nextUrl = new URL(
+            `${location.pathname}${location.search}${location.hash}`,
+            window.location.origin
+          );
+          nextUrl.searchParams.set("timestamp", nextTimestamp.toString());
+
+          next = nextUrl.toString().substring(window.location.origin.length);
+        }
+      }
+
+      let previous: string | undefined = undefined;
+      if (urlTimestamp) {
+        const previousUrl = new URL(
+          `${location.pathname}${location.search}${location.hash}`,
+          window.location.origin
+        );
+        previousUrl.searchParams.delete("timestamp");
+
+        previous = previousUrl.toString().substring(window.location.origin.length);
+      }
+      // fetch the current page of transactions in reverse using the timestamp of the last transaction
+      const previousResponse = await mirrorNodeClient.GET(
+        "/api/v1/contracts/{contractIdOrAddress}/results",
+        {
+          params: {
+            path: {
+              contractIdOrAddress: deployment.addresses.troveManager
+            },
+            query: {
+              timestamp: [`gt:${timestamp}`],
+              order: "asc"
+            }
+          }
+        }
+      );
+      if (previousResponse.data?.links?.next) {
+        const previousTimestamp = getTimestampFromHref(previousResponse.data.links.next);
+        if (previousTimestamp) {
+          const previousUrl = new URL(
+            `${location.pathname}${location.search}${location.hash}`,
+            window.location.origin
+          );
+          previousUrl.searchParams.set("timestamp", previousTimestamp.toString());
+
+          previous = previousUrl.toString().substring(window.location.origin.length);
+        }
+      }
+
       setRedemptions(redemptions);
+      setPagination({
+        next,
+        previous
+      });
     };
 
     effect();
-  }, [deployment, accountIdString, mirrorNodeClient, store.hchfTokenAddress]);
+  }, [
+    deployment,
+    hchfTokenIdString,
+    accountIdString,
+    mirrorNodeClient,
+    store.hchfTokenAddress,
+    urlTimestamp,
+    location
+  ]);
+
+  const previousPageIcon = <Icon aria-label="Previous page" name="chevron-left" size="sm" />;
+  const nextPageIcon = <Icon aria-label="Next page" name="chevron-right" size="sm" />;
+
+  const shortUrlTimestamp = useMemo(() => {
+    if (!urlTimestamp) {
+      return;
+    }
+
+    return shortDateTimeFormat.format(parseFloat(urlTimestamp) * 1000);
+  }, [urlTimestamp]);
+  const longUrlTimestamp = useMemo(() => {
+    if (!urlTimestamp) {
+      return;
+    }
+
+    return fullDateTimeFormat.format(parseFloat(urlTimestamp) * 1000);
+  }, [urlTimestamp]);
 
   return (
     <Container>
       <Card>
-        <Heading>Redemptions</Heading>
+        <Heading
+          sx={{
+            float: "left"
+          }}
+        >
+          Redemptions
+        </Heading>
+
+        <Flex
+          sx={{
+            float: "right",
+            mt: 2,
+            mr: 2
+          }}
+        >
+          {pagination.previous ? (
+            <RouterLink to={pagination.previous} sx={{ p: 0, m: 0, display: "block" }}>
+              <Button as="span" variant="titleIcon">
+                {previousPageIcon}
+              </Button>
+            </RouterLink>
+          ) : (
+            <Button disabled variant="titleIcon">
+              {previousPageIcon}
+            </Button>
+          )}
+
+          {pagination.next ? (
+            <RouterLink to={pagination.next} sx={{ p: 0, m: 0, display: "block" }}>
+              <Button as="span" variant="titleIcon">
+                {nextPageIcon}
+              </Button>
+            </RouterLink>
+          ) : (
+            <Button disabled variant="titleIcon">
+              {nextPageIcon}
+            </Button>
+          )}
+        </Flex>
 
         <Paragraph
           sx={{
             mt: 1,
-            px: 3
+            px: 3,
+            clear: "both"
           }}
         >
-          Your HCHF redemption history.
+          {urlTimestamp ? (
+            <>
+              All redemptions on the network before{" "}
+              <Tooltip message={<Text>{longUrlTimestamp}</Text>}>{shortUrlTimestamp}</Tooltip>.
+            </>
+          ) : (
+            <>All redemptions on the network.</>
+          )}
         </Paragraph>
 
         {!redemptions ? (
@@ -253,7 +443,16 @@ export const RedemptionsPage: React.FC = () => {
               px: 3
             }}
           >
-            <ActionDescription>You haven't redeemed any HCHF for HBAR yet.</ActionDescription>
+            <ActionDescription>
+              {longUrlTimestamp ? (
+                <>
+                  There are no redemptions before{" "}
+                  <Tooltip message={<Text>{longUrlTimestamp}</Text>}>{shortUrlTimestamp}</Tooltip>.
+                </>
+              ) : (
+                <>Noboy has redeemed any HCHF for HBAR yet.</>
+              )}
+            </ActionDescription>
           </Box>
         ) : (
           <Box
@@ -263,20 +462,36 @@ export const RedemptionsPage: React.FC = () => {
               textAlign: "start",
               lineHeight: 1.15,
               my: 4,
-              px: 3
+              px: 3,
+              borderCollapse: "collapse"
             }}
           >
             <thead>
               <tr>
+                {/* account */}
+                <th
+                  sx={{
+                    textAlign: "start",
+                    paddingBottom: "0.5rem",
+                    verticalAlign: "top",
+                    p: 2
+                  }}
+                >
+                  <Tooltip message={<Text>The user who redeemed the HCHF for HBAR.</Text>}>
+                    Account
+                  </Tooltip>
+                </th>
+
                 {/* hchf */}
                 <th
                   sx={{
                     textAlign: "start",
                     paddingBottom: "0.5rem",
-                    verticalAlign: "top"
+                    verticalAlign: "top",
+                    p: 2
                   }}
                 >
-                  <Tooltip message={<Text>The amount of HCHF you paid.</Text>}>
+                  <Tooltip message={<Text>The amount of HCHF the user paid.</Text>}>
                     Redeemed
                     <Box sx={{ fontSize: [0, 1], fontWeight: "body", opacity: 0.5 }}>HCHF</Box>
                   </Tooltip>
@@ -287,10 +502,11 @@ export const RedemptionsPage: React.FC = () => {
                   sx={{
                     textAlign: "start",
                     paddingBottom: "0.5rem",
-                    verticalAlign: "top"
+                    verticalAlign: "top",
+                    p: 2
                   }}
                 >
-                  <Tooltip message={<Text>The amount of HBAR you received.</Text>}>
+                  <Tooltip message={<Text>The amount of HBAR user received.</Text>}>
                     Received
                     <Box sx={{ fontSize: [0, 1], fontWeight: "body", opacity: 0.5 }}>HBAR</Box>
                   </Tooltip>
@@ -301,7 +517,8 @@ export const RedemptionsPage: React.FC = () => {
                   sx={{
                     textAlign: "start",
                     paddingBottom: "0.5rem",
-                    verticalAlign: "top"
+                    verticalAlign: "top",
+                    p: 2
                   }}
                 >
                   <Tooltip message={<Text>The date and time of the transaction.</Text>}>
@@ -315,14 +532,41 @@ export const RedemptionsPage: React.FC = () => {
               {redemptions.map(redemption => {
                 const date = new Date(parseFloat(redemption.timestamp) * 1000);
                 const shortDate = shortDateTimeFormat.format(date);
-                const longDate = longDateTimeFormat.format(date);
+                const longDate = fullDateTimeFormat.format(date);
+                const isOwnedByUser = redemption.accountIdString === accountIdString;
 
                 return (
-                  <tr key={redemption.transactionId}>
+                  <tr
+                    key={redemption.transactionId}
+                    sx={
+                      isOwnedByUser
+                        ? {
+                            backgroundColor: "primary",
+                            color: "secondary",
+                            fontWeight: 500
+                          }
+                        : undefined
+                    }
+                  >
+                    {/* account */}
+                    <td
+                      sx={{
+                        p: 2
+                      }}
+                    >
+                      <Link
+                        href={`${chain.hashscanBaseUrl}/account/${redemption.accountIdString}`}
+                        target="_blank"
+                      >
+                        {redemption.accountIdString}
+                        {isOwnedByUser && <> (you)</>}
+                      </Link>
+                    </td>
+
                     {/* hchf */}
                     <td
                       sx={{
-                        paddingBlock: "0.25rem"
+                        p: 2
                       }}
                     >
                       <Tooltip message={<Text>{redemption.amountOfHchf.toString()}</Text>}>
@@ -333,7 +577,7 @@ export const RedemptionsPage: React.FC = () => {
                     {/* hbar */}
                     <td
                       sx={{
-                        paddingBlock: "0.25rem"
+                        p: 2
                       }}
                     >
                       <Tooltip message={<Text>{redemption.amountOfHbar.toString()}</Text>}>
@@ -344,10 +588,15 @@ export const RedemptionsPage: React.FC = () => {
                     {/* date */}
                     <td
                       sx={{
-                        paddingBlock: "0.25rem"
+                        p: 2
                       }}
                     >
-                      <Tooltip message={<Text>{longDate}</Text>}>{shortDate}</Tooltip>
+                      <Link
+                        href={`${chain.hashscanBaseUrl}/transaction/${redemption.transactionId}`}
+                        target="_blank"
+                      >
+                        <Tooltip message={<Text>{longDate}</Text>}>{shortDate}</Tooltip>
+                      </Link>
                     </td>
                   </tr>
                 );
