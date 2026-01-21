83aa3816eee43130050d120d6115ba9c1311b5d8
Marko Gracin <marko@appswithlove.com>
2026-01-21 09:33:13 +0100

    fix(frontend): 🐛temporary hide trove tabs until borrow/repay/withdraw is better documented

--- packages/dev-frontend/src/components/Trove/Trove.tsx
+++ packages/dev-frontend/src/components/Trove/Trove.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { TroveManager } from "./TroveManager";
 import { ReadOnlyTrove } from "./ReadOnlyTrove";
 import { NoTrove } from "./NoTrove";
@@ -7,16 +8,16 @@ import { RedeemedTrove } from "./RedeemedTrove";
 import { useTroveView } from "./context/TroveViewContext";
 import { LiquidatedTrove } from "./LiquidatedTrove";
 import { Decimal } from "@liquity/lib-base";
-import { Tabs } from "../Tabs";
-import { useLiquitySelector } from "@liquity/lib-react";
-import { InfoMessage } from "../InfoMessage";
-import { CollateralSurplusAction } from "../CollateralSurplusAction";
-import { Borrow } from "./Borrow";
-import { CurrentUserTrove } from "./CurrentUserTrove";
-import { Repay } from "./Repay";
-import { Withdraw } from "./Withdraw";
+// import { Tabs } from "../Tabs";
+// import { useLiquitySelector } from "@liquity/lib-react";
+// import { InfoMessage } from "../InfoMessage";
+// import { CollateralSurplusAction } from "../CollateralSurplusAction";
+// import { Borrow } from "./Borrow";
+// import { CurrentUserTrove } from "./CurrentUserTrove";
+// import { Repay } from "./Repay";
+// import { Withdraw } from "./Withdraw";
 
-const TroveView: React.FC = props => {
+export const Trove: React.FC = props => {
   const { view } = useTroveView();
 
   switch (view) {
@@ -45,55 +46,55 @@ const TroveView: React.FC = props => {
   }
 };
 
-export const Trove: React.FC = () => {
-  const state = useLiquitySelector(state => {
-    return {
-      trove: state.trove
-    };
-  });
-
-  return (
-    <div role="presentation">
-      {state.trove.status === "closedByLiquidation" && !state.trove.isEmpty ? (
-        <>
-          <InfoMessage title="Your Trove has been liquidated.">
-            Please reclaim your remaining collateral before opening a new Trove.
-          </InfoMessage>
-          <CollateralSurplusAction />
-        </>
-      ) : state.trove.status === "closedByRedemption" && !state.trove.isEmpty ? (
-        <>
-          <InfoMessage title="Your Trove has been redeemed.">
-            Please reclaim your remaining collateral before opening a new Trove.
-          </InfoMessage>
-          <CollateralSurplusAction />
-        </>
-      ) : (
-        <>
-          {!state.trove.isEmpty && <CurrentUserTrove userTrove={state.trove} />}
-
-          <Tabs
-            label="Trove Operations"
-            tabs={[
-              {
-                title: "Borrow",
-                content: <Borrow />,
-                disabled: !state.trove.isEmpty
-              },
-              {
-                title: "Repay",
-                content: <Repay />,
-                disabled: state.trove.isEmpty
-              },
-              {
-                title: "Withdraw",
-                content: <Withdraw />,
-                disabled: state.trove.isEmpty
-              }
-            ]}
-          />
-        </>
-      )}
-    </div>
-  );
-};
+// export const Trove: React.FC = () => {
+//   const state = useLiquitySelector(state => {
+//     return {
+//       trove: state.trove
+//     };
+//   });
+//
+//   return (
+//     <div role="presentation">
+//       {state.trove.status === "closedByLiquidation" && !state.trove.isEmpty ? (
+//         <>
+//           <InfoMessage title="Your Trove has been liquidated.">
+//             Please reclaim your remaining collateral before opening a new Trove.
+//           </InfoMessage>
+//           <CollateralSurplusAction />
+//         </>
+//       ) : state.trove.status === "closedByRedemption" && !state.trove.isEmpty ? (
+//         <>
+//           <InfoMessage title="Your Trove has been redeemed.">
+//             Please reclaim your remaining collateral before opening a new Trove.
+//           </InfoMessage>
+//           <CollateralSurplusAction />
+//         </>
+//       ) : (
+//         <>
+//           {!state.trove.isEmpty && <CurrentUserTrove userTrove={state.trove} />}
+//
+//           <Tabs
+//             label="Trove Operations"
+//             tabs={[
+//               {
+//                 title: "Borrow",
+//                 content: <Borrow />,
+//                 disabled: !state.trove.isEmpty
+//               },
+//               {
+//                 title: "Repay",
+//                 content: <Repay />,
+//                 disabled: state.trove.isEmpty
+//               },
+//               {
+//                 title: "Withdraw",
+//                 content: <Withdraw />,
+//                 disabled: state.trove.isEmpty
+//               }
+//             ]}
+//           />
+//         </>
+//       )}
+//     </div>
+//   );
+// };
